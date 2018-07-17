8acc25c2d9f4b6c33dbab1ede3b81e167431dc4b
abradle <you@example.com>
2018-07-17 13:42:21 +0100

    Added the batch runners

---
+++ iridium/run_batch_3.sh
@@ -0,0 +1,8 @@
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_0/2mcp_ARG52_NH1_781 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_1/1lpz_ASP189_OD1_3562 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1hgh-1_GLY135_O_1985 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_3/1ydr_VAL123_N_1823 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_4/1ulb_GLU201_OE1_3136 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_5/1l2s_ALA318_O_4782 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_6/1hgj_ASN137_N_1999 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_7/1fh9_GLU233_OE2_3481 && frag_duck run.yaml > out.log
