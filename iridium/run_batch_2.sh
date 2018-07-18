c16b6cfd026cfaac143a5a7e834207da1890706a
abradle <you@example.com>
2018-07-18 16:21:35 +0100

    Update the batching

--- iridium/run_batch_2.sh
+++ iridium/run_batch_2.sh
@@ -1,8 +1,8 @@
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_0/1yv3_LEU262_O_3916 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_1/1l7f_GLU227_OE1_2279 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1fh8_GLN203_OE1_3004 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_3/1v0p_ASP85_OD1_1410 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_4/1rob_PHE120_N_1789 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_5/1k1j_ASP189_OD2_2471 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_6/1hgi_TYR98_OH_1438 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_7/1fh9_ASN126_ND2_1856 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1q1g_GLU184_OE2_2791 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1k3u_SER235_OG_3501 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1f0t_ASP189_OD1_2470 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1ydr_VAL123_N_1823 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1n46_ARG316_NH1_1636 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1ivd_ARG118_NH1_569 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1hgh-1_SER136_OG_1992 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1f0u_SER190_OG_2481 && frag_duck run.yaml > out.log
--- iridium/run_batch_3.sh
+++ iridium/run_batch_3.sh
@@ -1,8 +1,8 @@
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_0/2mcp_ARG52_NH1_781 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_1/1lpz_ASP189_OD1_3562 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1hgh-1_GLY135_O_1985 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_3/1ydr_VAL123_N_1823 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_4/1ulb_GLU201_OE1_3136 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_5/1l2s_ALA318_O_4782 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_6/1hgj_ASN137_N_1999 && frag_duck run.yaml > out.log
-cd /home/ubuntu/duck-benchmark/iridium/BATCH_7/1fh9_GLU233_OE2_3481 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1n2v_ASP156_OD2_2255 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1hgi_GLY135_O_1984 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1exa_SER289_OG_1722 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1v0p_ASP85_OD1_1410 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1l7f_ARG118_NH1_568 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1hwi_LYS691_NZ_9397 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1fhd-2_GLU127_OE2_1871 && frag_duck run.yaml > out.log
+cd /home/ubuntu/duck-benchmark/iridium/BATCH_2/1exa_MET272_SD_1437 && frag_duck run.yaml > out.log
