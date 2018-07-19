eef44524c734b462661ab0473acc3c6e0be59b44
Ubuntu <ubuntu@ip-172-31-16-14.ec2.internal>
2018-07-19 12:35:20 +0000

    Refactor out

--- iridium/get_res.py
+++ iridium/get_res.py
@@ -31,35 +31,6 @@ def get_wqb_simple(file_duck_dat):
     Wqb_value = Wqb_max - Wqb_min
     return(Wqb_value, data, Wqb_min)
 
-
-# TODO Improved method for finding WQB
-
-def get_Wqb_value(file_duck_dat):
-    f = open(file_duck_dat, 'r')
-    data = []
-    for line in f:
-        a = line.split()
-        data.append([float(a[1]), float(a[3]), float(a[5]), float(a[8])])
-    f.close()
-    data = np.array(data[1:])
-    Work = data[:, 3]
-    seg_size = 200
-    half_seg_size = seg_size // 2
-    half_seg_size_plus = half_seg_size + 1
-
-    # split it into segments of 200 points
-    num_segments = int(len(data) / seg_size)
-    # alayze each segment to see if minimum in the segment is the local minimum
-    # local minimum is the point with the lowest value of 200 neighbouring points
-    # first local minumum is miminum used later to duck analysis
-    print(Work)
-    Wqb_min = min(Work[:300])
-    sub_max_data = data[300:]
-    sub_max_Work = sub_max_data[:, 3]
-    Wqb_max = max(sub_max_Work)
-    Wqb_value = Wqb_max - Wqb_min
-    return (Wqb_value, data, Wqb_min)
-
 def get_Wqb_value_all(input_dir,data_dir):
     file_list = []
     for fil in os.listdir(input_dir):
